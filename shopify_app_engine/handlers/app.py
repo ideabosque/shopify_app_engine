36bfd3a95cee79a16ce384c19a384650cb970062
Barry Liu <barry.y.liu@hotmail.com>
2026-01-28 22:32:56 +0800

    [U] Optimization

--- shopify_app_engine/handlers/app.py
+++ shopify_app_engine/handlers/app.py
@@ -13,8 +13,9 @@ import boto3
 from boto3.dynamodb.conditions import Attr, Key
 from graphene import ResolveInfo
 
-from .utils import GraphqlSchemaUtility
 from .config import Config
+from .utils import GraphqlSchemaUtility
+
 
 class App(object):
     platform = "shopify"
@@ -26,7 +27,9 @@ class App(object):
     app_id = None
     graphql_schema_utility = None
 
-    def __init__(self, context: Dict[str, Any], logger: logging.Logger, **setting: Dict[str, Any]):
+    def __init__(
+        self, context: Dict[str, Any], logger: logging.Logger, **setting: Dict[str, Any]
+    ):
         try:
             self._initialize_config(setting)
             self._initialize_aws_services(setting)
@@ -38,22 +41,25 @@ class App(object):
             log = traceback.format_exc()
             logger.error(log)
             raise e
+
     def _initialize_config(self, setting: Dict[str, Any]) -> None:
         pass
 
     def _initialize_aws_services(self, setting: Dict[str, Any]) -> None:
         pass
 
-    def _initialize_graphql_schema_utility(self, logger: logging.Logger, setting: Dict[str, Any]) -> None:
+    def _initialize_graphql_schema_utility(
+        self, logger: logging.Logger, setting: Dict[str, Any]
+    ) -> None:
         self.graphql_schema_utility = GraphqlSchemaUtility(logger, **setting)
         self.app_id = None
 
     def install_app_config(self, app_id):
-        config = self.setting.get("app_settings",{}).get(app_id)
+        config = self.setting.get("app_settings", {}).get(app_id)
         variables = {
             "appId": app_id,
             "platform": self.platform,
-            "configuration": config
+            "configuration": config,
         }
         response = self.graphql_schema_utility.request_graphql(
             context=self.context,
@@ -62,13 +68,13 @@ class App(object):
             graphql_operation_name="insertUpdateAppConfig",
             graphql_operation_type="Mutation",
             class_name="AppCoreEngine",
-            variables=variables
+            variables=variables,
         )
 
     def install_app(self, **params):
         app_id = params.get("app_id")
         self.install_app_config(app_id)
-        config = self.setting.get("app_settings",{}).get(app_id)
+        config = self.setting.get("app_settings", {}).get(app_id)
         target_id = self.get_target_id(params.get("shop"))
         variables = {
             "appId": params.get("app_id"),
@@ -87,7 +93,7 @@ class App(object):
             graphql_operation_name="insertUpdateApp",
             graphql_operation_type="Mutation",
             class_name="AppCoreEngine",
-            variables=variables
+            variables=variables,
         )
 
     def get_app(self, app_id, shop):
@@ -104,20 +110,17 @@ class App(object):
             graphql_operation_name="app",
             graphql_operation_type="Query",
             class_name="AppCoreEngine",
-            variables=variables
+            variables=variables,
         )
-        
+
         app = None
         if response is not None:
             app = response
         return app
-    
+
     def get_shop_apps(self, shop):
         target_id = self.get_target_id(shop)
-        variables = {
-            "targetId": target_id,
-            "platform": self.platform
-        }
+        variables = {"targetId": target_id, "platform": self.platform}
 
         response = self.graphql_schema_utility.request_graphql(
             context=self.context,
@@ -126,19 +129,19 @@ class App(object):
             graphql_operation_name="appList",
             graphql_operation_type="Query",
             class_name="AppCoreEngine",
-            variables=variables
+            variables=variables,
         )
 
         app_list = None
         if response is not None and response["appList"] is not None:
             app_list = response["appList"]
         return app_list
+
     def get_access_token(self, **params):
         pass
 
     def get_target_id(self, shop):
         return shop.replace(".myshopify.com", "")
-
 
     def get_app_by_shop(self, shop, app_id=None):
         target_id = self.get_target_id(shop)
@@ -147,6 +150,6 @@ class App(object):
             return app
         shop_apps = self.get_shop_apps(shop)
         for shop_app in shop_apps:
-            Config.save_app(target_id, shop_app["appId"],shop_app)
+            Config.save_app(target_id, shop_app["appId"], shop_app)
 
         return Config.get_app(target_id, app_id)
--- shopify_app_engine/handlers/utils.py
+++ shopify_app_engine/handlers/utils.py
@@ -5,17 +5,18 @@ from __future__ import print_function
 __author__ = "bibow"
 
 import logging
+import re
 import time
 import traceback
-import re
 from typing import Any, Dict, List, Optional
 
 import boto3
 from boto3.dynamodb.conditions import Attr, Key
 from graphene import ResolveInfo
-
 from silvaengine_utility import Graphql
+
 from .config import Config
+
 
 class GraphqlSchemaUtility(object):
     logger = None
@@ -30,6 +31,7 @@ class GraphqlSchemaUtility(object):
             log = traceback.format_exc()
             self.logger.error(log)
             raise e
+
     def _initialize_aws_clients(self, setting: Dict[str, Any]) -> None:
         pass
 
@@ -45,15 +47,15 @@ class GraphqlSchemaUtility(object):
         query: str = None,
     ):
         try:
-            result =  Graphql.request_graphql(
+            result = Graphql.request_graphql(
                 context=context,
                 module_name=module_name,
                 function_name=function_name,
-                graphql_operation_type=graphql_operation_type,
-                graphql_operation_name=graphql_operation_name,
+                operation_type=graphql_operation_type,
+                operation_name=graphql_operation_name,
                 class_name=class_name,
                 variables=variables,
-                query=query
+                query=query,
             )
         except Exception as e:
             # log = traceback.format_exc()
@@ -61,8 +63,7 @@ class GraphqlSchemaUtility(object):
             return None
         return result
 
-        
-    
+
 class BuildContext(object):
     custom_context = None
     function_name = None
@@ -73,25 +74,19 @@ class BuildContext(object):
     log_group_name = None
     log_stream_name = None
     client_context = None
-    identity = {
-        'cognito_identity_id': None,
-        'cognito_identity_pool_id': None
-    }
-    
+    identity = {"cognito_identity_id": None, "cognito_identity_pool_id": None}
+
     def __init__(self, event, context):
         self.custom_context = {
-            'function_name': None,
-            'function_version': None,
-            'invoked_function_arn': None,
-            'memory_limit_in_mb': None,
-            'aws_request_id': None,
-            'log_group_name': None,
-            'log_stream_name': None,
-            'client_context': None,
-            'identity': {
-                'cognito_identity_id': None,
-                'cognito_identity_pool_id': None
-            }
+            "function_name": None,
+            "function_version": None,
+            "invoked_function_arn": None,
+            "memory_limit_in_mb": None,
+            "aws_request_id": None,
+            "log_group_name": None,
+            "log_stream_name": None,
+            "client_context": None,
+            "identity": {"cognito_identity_id": None, "cognito_identity_pool_id": None},
         }
         if len(context) > 0:
             self.custom_context = context
@@ -106,7 +101,9 @@ class BuildContext(object):
             self.identity = self.custom_context.get("identity")
         else:
             fn_configurations = event.get("fnConfigurations", {})
-            self.custom_context["invoked_function_arn"] = fn_configurations.get("invoked_function_arn")
+            self.custom_context["invoked_function_arn"] = fn_configurations.get(
+                "invoked_function_arn"
+            )
             self.custom_context["function_name"] = fn_configurations.get("function")
             self.function_name = fn_configurations.get("function")
             self.invoked_function_arn = fn_configurations.get("invoked_function_arn")
@@ -114,7 +111,7 @@ class BuildContext(object):
     # @staticmethod
     # def function_name(self):
     #     return self.custom_context.get("function_name")
-    
+
     # @staticmethod
     # def invoked_function_arn(self):
     #     return self.custom_context.get("invoked_function_arn")
@@ -122,11 +119,11 @@ class BuildContext(object):
     # @staticmethod
     # def function_version(self):
     #     return self.custom_context.get("function_version")
-    
+
     # @staticmethod
     # def memory_limit_in_mb(self):
     #     return self.custom_context.get("memory_limit_in_mb")
-    
+
     # @staticmethod
     # def aws_request_id(self):
     #     return self.custom_context.get("aws_request_id")
@@ -134,16 +131,15 @@ class BuildContext(object):
     # @staticmethod
     # def log_group_name(self):
     #     return self.custom_context.get("log_group_name")
-    
+
     # @staticmethod
     # def log_stream_name(self):
     #     return self.custom_context.get("log_stream_name")
-    
+
     # @staticmethod
     # def client_context(self):
     #     return self.custom_context.get("client_context")
-    
+
     # @staticmethod
     # def identity(self):
     #     return self.custom_context.get("identity")
-    
